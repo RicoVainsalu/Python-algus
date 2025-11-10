90f00274ad401f5bd83e4b377c5229e2f01c4837
RicoVainsalu <rvainsalu@hkhk.edu.ee>
2025-11-10 10:35:07 +0200

    esimene fail

---
+++ h01.py
@@ -0,0 +1,39 @@
+# Rico Vainsalu
+# Harjutus 01
+# print('Tere \"suur\" maailm!')
+#Kolmnurk
+import turtle
+turtle.up()
+turtle.goto(-400,300)
+turtle.down()
+for i in range(3):
+    turtle.forward(200)
+    turtle.right(120)
+
+
+
+#Nimetäht R
+turtle.up()
+turtle.goto(-50,-100)
+turtle.pendown()
+for i in range(6):
+    turtle.penup()
+    
+    
+    
+    import turtle
+
+t = turtle.Turtle()
+
+# Joonista R täht
+t.left(90)
+t.forward(100) # püstjoon
+t.right(90)
+t.circle(-25, 180) # ülemine poolring
+t.left(135)
+t.forward(70) # diagonaal alla
+
+t.hideturtle()
+turtle.done()
+
+
