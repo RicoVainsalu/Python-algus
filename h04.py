5f4d4248e2a73bec82cd53a837ac65d3d1323cc8
RicoVainsalu <rvainsalu@hkhk.edu.ee>
2025-11-18 13:58:45 +0200

    fhdhdf

---
+++ h04.py
@@ -0,0 +1,24 @@
+# Ülesanded 4
+
+#Raamatute allahindlus
+
+
+# Raamatute allahindlus
+# Raamatupoes on 30% soodusmüük.
+# Kirjuta programm, mis küsib kasutajalt soovitud raamatute arvu ja arvutab nende kogumaksumuse,
+#kui iga raamatu tavahind on 12,53€.
+# Väljasta lause, kasutades f-string vormindamist ja ümardamist 2 komakohta
+# Näide:
+# Kasutaja sisestab: 3
+# Programm väljastab: 3 raamatu hind soodustusega on 26.25€
+
+
+ale = 0.3
+raamatute_arv = int(input("soovitud raamatute arv: "))
+hind = 12.53
+summa = raamatute_arv * (hind-hind*ale)
+
+
+
+print(f"{raamatute_arv} raamatu hind soodustusega on {summa:0.2f}€")
+
